901707e1f97a02065b86b70a3ff9c01315c29e82
Sodhi-Amrita <amrita.kaur.sodhi.com>
2023-04-11 01:39:21 -0400

    first commit

--- config/settings.js
+++ config/settings.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 const username = process.env.USER_NAME;
 const password = process.env.PASS_WORD;
 module.exports = {
-    mongoDBUrl: 'mongodb+srv://${username}:${password}@cluster0.ke9tebn.mongodb.net/User?retryWrites=true&w=majority'
+    mongoDBUrl: `mongodb+srv://${username}:${password}@cluster0.ke9tebn.mongodb.net/User?retryWrites=true&w=majority`
 }
