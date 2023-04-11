dbedb8365361e8e1465eccdceee732d67df0348f
Sodhi-Amrita <amrita.kaur.sodhi.com>
2023-04-11 01:32:26 -0400

    first commit

---
+++ config/settings.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const username = process.env.USER_NAME;
+const password = process.env.PASS_WORD;
+module.exports = {
+    mongoDBUrl: 'mongodb+srv://${username}:${password}@cluster0.ke9tebn.mongodb.net/User?retryWrites=true&w=majority'
+}
